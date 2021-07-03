7adee1516f2b7705831a9b86fdf213eb5ed2f1b6
Emas <emas.dev@gmail.com>
2021-07-03 13:18:31 -0500

    Add checkboxes

--- docs/steps.md
+++ docs/steps.md
@@ -1,11 +1,17 @@
 # IDM Site
 
-Additional description about the project and its features.
+This is a website for .
 
-## Steps
+## Main Steps
 
 - Create repository
 - Add bootstrap template
 - Add info from idm-mexico.com
 - Add menu
 - Add gallery
+- Add ratings
+- Change radio buttons for checkboxes in OrdenDeTrabajo
+
+## Present Steps
+
+- Add checkboxes for teeth
--- js/app.js
+++ js/app.js
@@ -1,14 +1,48 @@
-$(function () {
+$(function() {
+  $("#calendario").simpleCalendar();
   // Disable default behavior
-  $(".form-check-input").click(function (e) {
+  $(".form-check-input").click(function(e) {
     e.preventDefault();
   });
 
-  $(".form-check-input").on("mouseup", function () {
+  $(".form-check-input").on("mouseup", function() {
     if ($(this).is(":checked")) {
       $(this).prop("checked", false);
     } else {
       $(this).prop("checked", true);
     }
   });
+
+  //generateDientes(document.getElementById("dientes"));
+
+  function generateDientes(container) {
+    const num_dientes = 16;
+    const rows = [2];
+    var html = "";
+
+    appendHeader();
+    appendRow();
+    $.each(rows, function(index, value) {});
+
+    appendFooter();
+
+    function appendHeader() {
+      html += "";
+    }
+
+    function appendRow() {
+      html +=
+        '<div class=" d-flex flex-column text-center align-items-center">';
+      $;
+    }
+    function appendFooter() {
+      html += "</div>";
+    }
+    function appendDiente() {
+      html +=
+        '        <input class="form-check-input" type="checkbox" value="" id="diente1"><label class="form-check-label" for="diente1">diente1</label>';
+    }
+
+    container.innerHTML = html;
+  }
 });
