0c56b045c0307b932089518e75af26c58d7fb903
Emas <emas.dev@gmail.com>
2021-07-14 13:21:11 -0500

    Add doctor and paciente data

--- js/validation.js
+++ js/validation.js
@@ -34,6 +34,36 @@
   });
 
   function tabToStep(index) {
+    if (index == 1) {
+      function print() {
+        // obtener datos
+        const doctor = {
+          nombre: $("#doctor-nombre").val(),
+          apellidos: $("#doctor-apellidos").val(),
+          telefono: $("#doctor-telefono").val(),
+          email: $("#doctor-email").val()
+        };
+
+        var nombre = doctor.nombre + " " + doctor.apellidos;
+
+        $("#confirmar-doctor").find(".nombre-label").html(nombre);
+        $("#confirmar-doctor").find(".email-label").html(doctor.email);
+        $("#confirmar-doctor").find(".telefono-label").html(doctor.telefono);
+      }
+
+      const paciente = {
+        nombre: $("#paciente-nombre").val(),
+        apellidos: $("#paciente-apellidos").val(),
+        telefono: $("#paciente-telefono").val(),
+        email: $("#paciente-email").val()
+      };
+
+      nombre = paciente.nombre + " " + paciente.apellidos;
+
+      $("#confirmar-paciente").find(".nombre-label").html(nombre);
+      $("#confirmar-paciente").find(".email-label").html(paciente.email);
+      $("#confirmar-paciente").find(".telefono-label").html(paciente.telefono);
+    }
     const stepElm = "#step-" + index;
     $(".step").hide();
     $(".step").removeClass("d-none");
