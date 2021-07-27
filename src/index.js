9c06f5370f4ba8e64a3725b949729c184e5b9ee6
ESTACION-UNO <emas.dev@gmail.com>
2021-07-27 18:58:22 -0500

    prepare for google app engine

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 3500;
+const PORT = process.env.PORT || 3500;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(require("./routes/index"));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(port, () => {
-  console.log("Iniciar servidor puerto " + port);
+app.listen(PORT, () => {
+  console.log("Iniciar servidor puerto " + PORT);
 });
