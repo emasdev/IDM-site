084ed8e925f57df07ece2dbab38354e9070f87aa
ESTACION-UNO <emas.dev@gmail.com>
2021-07-29 17:53:34 -0500

    Finished formulario citas

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,15 @@ const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
 const router = express.Router();
 
+const CLIENT_ID =
+"149709447482-ol5t6u6asf93rkfq3q28g37shpb1ocrg.apps.googleusercontent.com";
+const CLIENT_SECRET = "Z4zVbYqxJtreN57nPh9q6k1y";
+const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+const REFRESH_TOKEN =
+"1//04hw6ROjAkx0WCgYIARAAGAQSNwF-L9IrB1cu_tomCzQLRB-5CaHuVCGwo48l9m4ug4tqdX07zkQaL2aNGPCd7pgqGHUbGHk4fbY";
+
+
+
 router.post("/send-email-info", (req, res) => {
     console.log(req.body)
     const { nombre, email, telefono, mensaje } = req.body;
@@ -16,16 +25,10 @@ router.post("/send-email-info", (req, res) => {
   <p>${mensaje}</p>
   `;
 
-    const CLIENT_ID =
-        "149709447482-ol5t6u6asf93rkfq3q28g37shpb1ocrg.apps.googleusercontent.com";
-    const CLIENT_SECRET = "Z4zVbYqxJtreN57nPh9q6k1y";
-    const REDIRECT_URI = "https://developers.google.com/oauthplayground";
-    const REFRESH_TOKEN =
-        "1//04hw6ROjAkx0WCgYIARAAGAQSNwF-L9IrB1cu_tomCzQLRB-5CaHuVCGwo48l9m4ug4tqdX07zkQaL2aNGPCd7pgqGHUbGHk4fbY";
-    const oAuth2Client = new google.auth.OAuth2(
-        CLIENT_ID,
-        CLIENT_SECRET,
-        REDIRECT_URI
+  const oAuth2Client = new google.auth.OAuth2(
+    CLIENT_ID,
+    CLIENT_SECRET,
+    REDIRECT_URI
     );
 
     oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
@@ -67,21 +70,34 @@ router.post("/send-email-cita", (req, res) => {
     const { doctor, paciente, orden_de_estudio } = req.body;
     console.log(req.body);
 
-    res.json({ status: 'probando' })
-    return;
+    var contentHTML = `
+    <h2>Doctor:</h2>
+    <div>Nombre: ${doctor.nombre}</div>
+    <div>Apellidos: ${doctor.apellidos}</div>
+    <div>Teléfono: ${doctor.telefono}</div>
+    <div>Email: ${doctor.email}</div>
+    </br>
+    <h2>Paciente:</h2>
+    <div>Nombre: ${paciente.nombre}</div>
+    <div>Apellidos: ${paciente.apellidos}</div>
+    <div>Teléfono: ${paciente.telefono}</div>
+    <div>Email: ${paciente.email}</div>
+    </br>`;
 
+    if(orden_de_estudio.length > 0) {
+        contentHTML += '<h2>Orden de Trabajo:</h2>';
+        contentHTML += '<ul>';
+        orden_de_estudio.forEach(element => {
+            contentHTML += '<li>' + element + '</li>';
+        });
+        contentHTML += '</ul>';
+    }
 
-    const CLIENT_ID =
-        "149709447482-ol5t6u6asf93rkfq3q28g37shpb1ocrg.apps.googleusercontent.com";
-    const CLIENT_SECRET = "Z4zVbYqxJtreN57nPh9q6k1y";
-    const REDIRECT_URI = "https://developers.google.com/oauthplayground";
-    const REFRESH_TOKEN =
-        "1//04hw6ROjAkx0WCgYIARAAGAQSNwF-L9IrB1cu_tomCzQLRB-5CaHuVCGwo48l9m4ug4tqdX07zkQaL2aNGPCd7pgqGHUbGHk4fbY";
     const oAuth2Client = new google.auth.OAuth2(
         CLIENT_ID,
         CLIENT_SECRET,
         REDIRECT_URI
-    );
+        );
 
     oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
@@ -113,7 +129,7 @@ router.post("/send-email-cita", (req, res) => {
     }
 
     sendMail()
-        .then(result => res.status(200).send("enviado"))
+        .then(result =>  res.json({ status: 'enviado' }))
         .catch(error => console.log(error.message));
 });
 
