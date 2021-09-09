c9b0295be3f07d06cd12c65e29b9d414a1f0ea21
Emanuel González Irigoyen <emas.dev@gmail.com>
2021-09-09 12:53:33 -0500

    add reviews

--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ const PORT = process.env.PORT || 3500;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(function (req, res, next) {
+
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3500');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  next();
+});
 app.use(require("./routes/index"));
 app.use(express.static(path.join(__dirname, "public")));
 
